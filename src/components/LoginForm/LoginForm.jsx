565ca8c180cdf891ff547dafa49f423db570d826
Eben Stine <ebenstine@gmail.com>
2022-01-28 22:42:51 -0600

    updated login and login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,13 +1,133 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom'
+import { Button, Paper, Grid, Container, Typography, Card, CardHeader, CardContent, makeStyles, ThemeProvider } from '@material-ui/core';
+import { TextField, FormControl } from '@mui/material'
+//change of styling with MUI components
+const useStyles = makeStyles((theme) => ({
+
+  root: {
+    paddingTop: '5em',
+  
+      '& .MuiTextField-root': {
+          margin: theme.spacing(1),
+          color: '#2a4f64',
+          
+          '&:hover fieldset': {
+            borderColor: '#ffab5c'
+          }
+          //width: '25ch'
+      },
+      '& label.Mui-focused': {
+          color: '#3b95ac',
+          borderColor: '#ffab5c',
+      },
+      '& .MuiInput-underline:after': {
+          borderBottomColor: '#3b95ac',
+      }, 
+          
+          
+          
+      
+      "& .MuiOutlinedInput-input": {
+          color: "#2a4f64",
+         
+          
+      },
+      //"& .Mui-disabled .MuiOutlinedInput-notchedOutline": {
+          //border: "2px solid #3b95ac"
+        //},
+        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+          border: "1.5px solid#3b95ac",
+          borderRadius: "3px 3px 3px 3px"
+        },
+      
+
+        
+      '& .MuiOutlinedInput-root': {
+          '& fieldset': {
+              color: '#3b95ac',
+              //paddingLeft: '4em'
+              
+          },
+          '& .Mui-selected': {
+
+             borderColor: '3b95ac'
+
+          },
+          '&:hover fieldset': {
+              border:' 1.5px solid #3b95ac' 
+          },  
+          '&:fieldset.Mui-focused': {
+              border:' 1.5px solid #3b95ac'
+          
+          },
+          
+          
+          
+          
+          
+        
+          
+          
+          
+      },
+  },
+ 
+   btn: {
+     marginTop: 30,
+     marginBottom: 25,
+     background: '#fff099',
+         color: '#2a4f64',
+         border: '1px solid #3b95ac',
+         '&:hover': {
+             background:'#fde76c',
+             },
+     paddingLeft: '1em',
+     paddingRight: '1em',
+     color: '#2a4f64' 
+   },
+   
+   signIn: {
+     marginBottom: 15,
+     color: '#2a4f64',
+     
+     
+   },
+   password: {
+     marginTop: 15,
+     
+     
+   },
+ 
+   
+ 
+ }))
+
+
+
+
 
 function LoginForm() {
+  //declaring classes for MUI styling so can target specific components to change
+  const classes = useStyles();
+
+  //creating local states
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  //grabbing error store
   const errors = useSelector(store => store.errors);
+
+  //declaring variable to use dispatch function
   const dispatch = useDispatch();
 
+  //declaring variable to utilize history function
+  const history = useHistory();
+
+
+  //if have correct login - on "sign in" redirect to home / otherwise render login error
   const login = (event) => {
     event.preventDefault();
 
@@ -19,47 +139,52 @@ function LoginForm() {
           password: password,
         },
       });
+      history.push('/user')
     } else {
       dispatch({ type: 'LOGIN_INPUT_ERROR' });
     }
   }; // end login
 
   return (
-    <form className="formPanel" onSubmit={login}>
-      <h2>Login</h2>
-      {errors.loginMessage && (
-        <h3 className="alert" role="alert">
-          {errors.loginMessage}
-        </h3>
-      )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            required
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn" type="submit" name="submit" value="Log In" />
-      </div>
-    </form>
+    <>
+      {/* mui container for side margins */}
+  
+      <Container align="center" className={classes.root}>
+        {/* grid for responsive design */}
+        
+        <Grid noValidate autoComplete="off" onSubmit={login}>
+
+          <Grid item lg={2} xs={6} sm={3} md={3}>
+            {/* login inputs / button */}
+            <Typography variant="h5" className={classes.signIn}>Sign In</Typography>
+          </Grid>
+          {errors.loginMessage && (
+            <h3 className="alert" role="alert">
+              {errors.loginMessage}
+            </h3>
+          )}
+
+          <Grid item lg={2} xs={6} sm={3} md={3}>
+            <TextField noValidate autoComplete="off" required variant="outlined" label="Username"  value={username}
+              onChange={(event) => setUsername(event.target.value)} />
+          </Grid>
+          <Grid item lg={2} xs={6} sm={3} md={3}>
+            <TextField className={classes.password} required label="Password" variant="outlined"  value={password}
+              type="password" noValidate autoComplete="off" onChange={(event) => setPassword(event.target.value)} />
+          </Grid>
+          
+            <Grid item lg={2} xs={6} sm={3} md={3}>
+              <Button variant="contained" className={classes.btn} align="center" onClick={login}>Go!</Button>
+            </Grid>
+          
+
+        </Grid>
+      
+      </Container>
+       
+      
+
+    </>
   );
 }
 
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,26 +1,68 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
+import { Button, Paper, Typography, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+
+  invite: {
+    color: '#2a4f64',
+
+  },
+
+  signUp: {
+    background: '#fff099',
+        color: '#2a4f64',
+        border: '1px solid #3b95ac',
+        '&:hover': {
+            background:'#fde76c',
+            },
+    paddingLeft: '1em',
+    paddingRight: '1em',
+    color: '#2a4f64',
+    marginBottom: '10em'
+  },
+
+
+
+  
+ //paper: {
+    margin: '4em auto',
+    background: 'linear-gradient(to right,  #9c9e9f 0%,#f6f6f6 100%)',
+    border: '1px solid #fdd377',
+    paddingBottom: '2em',
+    width: 650,
+    marginBottom: '10em'
+//},
+
+})
+
 
 function LoginPage() {
   const history = useHistory();
+  const {invite, signUp, paper} = useStyles();
 
   return (
-    <div>
+    <>
+  
       <LoginForm />
-
+      <Typography align="center" variant="h6" className={invite}>
+      No account yet?
+      </Typography>
+      <br></br>
       <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
+        <Button
+          variant="contained"
+          className={signUp}
           onClick={() => {
             history.push('/registration');
           }}
         >
-          Register
-        </button>
+          Make One!
+        </Button>
       </center>
-    </div>
+     
+    </>
   );
 }
 
